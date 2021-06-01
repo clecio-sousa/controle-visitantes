ca6aee72f4adb409b2f5029ee3d3ca50507c4d39
clecio1201 <clecio1201@hotmail.com>
2021-06-01 00:40:11 -0300

    code update

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse(" Olá, Mundo!!")
+    return HttpResponse(" Olá, Mundo!!!!")
 
 # Create your views here.
 
