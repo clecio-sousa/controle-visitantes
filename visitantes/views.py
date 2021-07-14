6480d016c0b7b6f860909b8127ae8df3ef85a9ab
clecio1201 <clecio1201@hotmail.com>
2021-07-14 20:11:02 -0300

    criacao do metodo POST e validacao do FORM

--- visitantes/views.py
+++ visitantes/views.py
@@ -7,6 +7,13 @@ def registrar_visitante(request):
     
     form = VisitanteForm()
     
+    if request.method == "POST":
+        form = VisitanteForm(request.POST)
+        
+        if form.is_valid():
+            form.save()      
+        
+    
     context = {
         "nome_pagina" : "Registrar Visitante",
         "form" : form        
