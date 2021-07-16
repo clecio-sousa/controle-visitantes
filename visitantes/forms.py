2d8daaf2953b321ca777add1d0378ec246df50db
clecio1201 <clecio1201@hotmail.com>
2021-07-16 05:57:53 -0300

    msg de erro no form

--- visitantes/forms.py
+++ visitantes/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.messages.api import error
 from django.forms import fields
 from django.forms.models import ModelForm #importando FORMS
 from visitantes.models import Visitante #criando FORM a partir de MODELS (from <NOME_DA_APP>.models import NOME_DA_CLASSE) 
@@ -7,6 +8,27 @@ class VisitanteForm(forms.ModelForm):
     class Meta:
         model = Visitante
         fields = "__all__" #representa todos os campos
+        
+        
+        # INICIO DAS MENSAGENS DE ERRO NO FORMULARIO
+        error_messages = {
+            "nome_completo" :{
+                "required": " O nome completo é obrigatório"
+            },
+             "cpf" :{
+                "required": " O CPF é obrigatório"
+            },
+            "data_nascimento" :{
+                "required": " A data de nascimento é obrigatória",
+                "invalid": "Informe um formato válido para a data de nascimento(DD/MM/AAAA)"
+            },
+             "numero_casa" :{
+                "required": "Informe o número da casa a ser visitada"
+            }
+            
+        }
+        #FIM DAS MENSAGENS DE ERRO NO FORMULARIO
+
         
         """" 
         CAMPOS SELECIONADOS
