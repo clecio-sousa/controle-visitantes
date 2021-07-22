4f9d124ee30fbdc13ddbf76399a9df8de53a24eb
clecio1201 <clecio1201@hotmail.com>
2021-07-22 01:09:03 -0300

    Adicionado url de finalizar visita

--- controle_visitantes/urls.py
+++ controle_visitantes/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from usuarios.views import index
-from visitantes.views import registrar_visitante, informacoes_visitante #importando a funcao registrar-visitante na VIEW
+from visitantes.views import registrar_visitante, informacoes_visitante, finalizar_visita #importando a funcao registrar-visitante na VIEW
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -20,5 +20,10 @@ urlpatterns = [
         "informacao-visitante/<int:id>/",
         informacoes_visitante,
         name="informacoes_visitante"
-         )
+         ),
+    
+    path("informacao-visitante/<int:id>/finalizar-visita",
+         finalizar_visita,
+         name="finalizar_visita"        
+    )
 ]
